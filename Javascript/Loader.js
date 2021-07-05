4ce1851bd5fa3dc6f2af41f1a1d097d79f41b7f5
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-07-05 00:50:22 -0300

    Index Planes + Servicios + 404 + 403 + SVG Images + htaccess

--- Javascript/Loader.js
+++ Javascript/Loader.js
@@ -1,41 +1,82 @@
-$( document ).ready(function(){
-    $('#header').load('/AralixDesigns/Page/header.html');
-    $('#footer').load('/AralixDesigns/Page/footer.html');
+$(document).ready(function () {
+  $("#header").load("/AralixDesigns/Page/header.html");
+  $("#footer").load("/AralixDesigns/Page/footer.html");
 });
 
+// Menu Mobile
+
 function burger_menu(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 
-    if (document.getElementById("links-mobile").style.transform == "translateY(0%)") {
-        document.getElementById("links-mobile").style.transform = "translateY(-120%)";
-    } else {
-        document.getElementById("links-mobile").style.transform = "translateY(-0%)";
-    }
+  if (
+    document.getElementById("links-mobile").style.transform == "translateY(0%)"
+  ) {
+    document.getElementById("links-mobile").style.transform =
+      "translateY(-120%)";
+  } else {
+    document.getElementById("links-mobile").style.transform = "translateY(-0%)";
+  }
 }
 
 
-function loginModal(){
-    $(".login-wrapper").css ("display", "flex");
-    $("body").css ("overflow", "hidden");
+
+// Login Modal
+
+function loginModal() {
+  $(".login-wrapper").css("display", "flex");
+  $("body").css("overflow", "hidden");
 }
-function closeModal(){
-    $(".login-wrapper").css ("display", "none");
-    $("body").css ("overflow", "auto");
+function closeModal() {
+  $(".login-wrapper").css("display", "none");
+  $("body").css("overflow", "auto");
 }
 
-window.onscroll = function() {scrollFunction()};
+
+
+// Scroll Function
+
+
+window.onscroll = function () {
+  scrollFunction();
+  scrollIndicator();
+};
+
+
+
+// Scroll to Top Button
 
 function scrollFunction() {
-    var mybutton = document.getElementById("scrollTop");
-    
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  var mybutton = document.getElementById("scrollTop");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+// Scroll Indicator
+
+function scrollIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var total_scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = total_scrolled + "%";
+
+  var progress_bar = document.getElementById("scroll-indicator");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    progress_bar.style.display = "block";
+  } else {
+    progress_bar.style.display = "none";
+  }
 }
